067c0b704c34ac4bd6a53e179f27a1fe0b01ef24
Matt Platts <mattplatts@gmail.com>
2019-04-11 15:42:09 -0400

    Add code for generating random mazes - first draft

--- js/maze.js
+++ js/maze.js
@@ -55,6 +55,7 @@ function convert(maze){
 */
 function renderGrid(){
 	var interim_maze = convert(maze);
+	//interim_maze=randomMaze();
 	var bindata = Array();
 	var binpills = Array();
 	var x=0;
@@ -201,4 +202,163 @@ function renderGrid(){
 	}
 	document.getElementById('mazeinner').innerHTML=innerStr;
 	return bindata;
+}
+
+// Randomly generated mazes
+function Maze(w, h){ 
+        this.w = (isNaN(w) || w < 5 || w > 999 ? 20 : w); 
+        this.h = (isNaN(h) || h < 5 || h > 999 ? 20 : h); 
+        this.map = new Array();
+        for(var mh = 0; mh < h; ++mh) { this.map[mh] = new Array(); for(var mw = 0; mw < w; ++mw) { this.map[mh][mw] = {'n':0,'s':0,'e':0,'w':0,'v':0}; } } 
+        this.dirs = ['n', 's', 'e', 'w'];
+        this.modDir = { 
+                'n' : { y : -1, x : 0, o : 's' },
+                's' : { y : 1, x : 0, o : 'n' },
+                'e' : { y : 0, x : -1, o : 'w' },
+                'w' : { y : 0, x : 1, o : 'e' }
+        };  
+
+        this.build(0, 0); 
+}
+
+Maze.prototype.toGrid = function(){
+        var grid = new Array();
+        for(var mh = 0; mh < (this.h * 2 + 1); ++mh) { 
+                grid[mh] = new Array(); 
+                for(var mw = 0; mw < (this.w * 2 + 1); ++mw) { 
+                        grid[mh][mw] = 0; } 
+                }   
+
+        for(var y = 0; y < this.h; ++ y){ 
+                var py = (y * 2) + 1;
+
+                for(var x = 0; x < this.w; ++x){
+                        var px = (x * 2) + 1;
+
+                        if(this.map[y][x].v==1) {
+                                grid[py][px] = 1;
+                        }   
+
+                        for(d in this.dirs){
+                                if(this.map[y][x][this.dirs[d]]==1) {
+                                         grid[(py+this.modDir[this.dirs[d]].y)][(px+this.modDir[this.dirs[d]].x)] = 1;
+                                }   
+                        }   
+                }   
+        }   
+
+        this.gridMap = grid;
+        this.gridW      = grid.length;
+        this.gridH      = grid[0].length;
+};
+
+Maze.prototype.build = function(x, y){ 
+        var x = 0;
+        var y = 0;
+
+        this.explore(x, y); 
+        this.toGrid();
+};
+
+Maze.prototype.explore = function(ex, ey){
+        this.dirs = sortRand(this.dirs);
+
+        for(d in this.dirs){
+                var nx = ex + this.modDir[this.dirs[d]].x;
+                var ny = ey + this.modDir[this.dirs[d]].y;
+
+                if(nx >= 0 && nx < this.w && ny >= 0 && ny < this.h && this.map[ny][nx].v==0){
+                        this.map[ey][ex][this.dirs[d]] = 1;
+                        this.map[ey][ex].v = 1;
+                        this.map[ny][nx][this.modDir[this.dirs[d]].o] = 1;
+
+                        this.explore(nx, ny);
+                }
+        }
+};
+
+
+function sortRand(a){
+        var out = new Array();
+        var l   = a.length;
+
+        for(x in a){
+                do { var p = Math.floor(Math.random() * (l * 1000)) % l; } while(typeof out[p]!='undefined');
+
+                out[p] = a[x];
+        }
+
+        return out;
+}
+
+function randomMaze(){
+
+        mazeMap = new Maze(10,8);
+
+	// remove top and bottom walls
+        mazeMap.gridMap.shift();
+        mazeMap.gridMap.pop();
+	//  remove left and right walls
+        for (i=0;i<mazeMap.gridMap.length;i++){
+                mazeMap.gridMap[i].pop();
+                mazeMap.gridMap[i].shift();
+
+        }
+
+        //document.getElementById("sourcear").innerHTML=mazeMap.gridMap.toString();
+        addGhostHome();
+	addPowerPills();
+        return mazeMap.gridMap;
+
+}
+
+function addGhostHome(){
+
+	mazeMap.gridMap[4][7]=1;
+	mazeMap.gridMap[4][6]=1;
+	mazeMap.gridMap[4][8]=1;
+	mazeMap.gridMap[4][9]=1;
+	mazeMap.gridMap[4][10]=1;
+	mazeMap.gridMap[4][11]=1;
+	mazeMap.gridMap[4][12]=1;
+
+	mazeMap.gridMap[5][7]=1;
+	mazeMap.gridMap[5][7]=0;
+	mazeMap.gridMap[5][8]=0;
+	mazeMap.gridMap[5][9]=5;
+	mazeMap.gridMap[5][10]=0;
+	mazeMap.gridMap[5][11]=0;
+	mazeMap.gridMap[5][12]=1;
+	
+	mazeMap.gridMap[6][6]=1;
+	mazeMap.gridMap[6][7]=0;
+	mazeMap.gridMap[6][8]=3;
+	mazeMap.gridMap[6][9]=3;
+	mazeMap.gridMap[6][10]=3;
+	mazeMap.gridMap[6][11]=0;
+	mazeMap.gridMap[6][12]=1;
+
+	mazeMap.gridMap[7][6]=1;
+	mazeMap.gridMap[7][7]=0;
+	mazeMap.gridMap[7][8]=0;
+	mazeMap.gridMap[7][9]=0;
+	mazeMap.gridMap[7][10]=0;
+	mazeMap.gridMap[7][11]=0;
+	mazeMap.gridMap[7][12]=1;
+
+	mazeMap.gridMap[8][6]=1;
+	mazeMap.gridMap[8][7]=1;
+	mazeMap.gridMap[8][8]=1;
+	mazeMap.gridMap[8][9]=1;
+	mazeMap.gridMap[8][10]=1;
+	mazeMap.gridMap[8][11]=1;
+	mazeMap.gridMap[8][12]=1;
+}
+	
+function addPowerPills(){
+	mazeMap.gridMap[0][0]=2;
+	mazeMap.gridMap[14][0]=2;
+	mazeMap.gridMap[0][18]=2;
+	mazeMap.gridMap[14][18]=2;
+
 }
